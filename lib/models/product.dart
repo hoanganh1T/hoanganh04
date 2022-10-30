fb8bb793a3fef9de90f07b9e2de5ab4a8000bf52
anh1910493 <104414856+anh1910493@users.noreply.github.com>
2022-10-30 09:13:27 +0700

    Xay dung trang gio hang

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/foundation.dart';
+
 class Product {
   final String? id;
   final String title;
   final String description;
-  final String price;
+  final double price;
   final String imageUrl;
-  final bool isFavorite;
+  final ValueNotifier<bool> _isFavorite;
 
   Product({
     this.id,
@@ -12,23 +14,53 @@ class Product {
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavorite = false,
-  });
+    isFavorite = false,
+  }) : _isFavorite = ValueNotifier(isFavorite);
+  set isFavorite(bool newValue) {
+    _isFavorite.value = newValue;
+  }
+
+  bool get isFavorite {
+    return _isFavorite.value;
+  }
+
+  ValueNotifier<bool> get isFavoriteListenable {
+    return _isFavorite;
+  }
+
   Product copyWith({
     String? id,
     String? title,
     String? description,
-    double? prince,
+    double? price,
     String? imageUrl,
     bool? isFavorite,
   }) {
     return Product(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      description: description ?? this.description,
-      price: price ?? this.price,
-      imageUrl: imageUrl ?? this.imageUrl,
-      isFavorite: isFavorite ?? this.isFavorite,
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        price: price ?? this.price,
+        imageUrl: imageUrl ?? this.imageUrl,
+        isFavorite: isFavorite ?? this.isFavorite);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'price': price,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  static Product fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      price: json['price'],
+      imageUrl: json['imageUrl'],
     );
   }
 }
