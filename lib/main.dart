7dddbbfe7f09bf755c3d29d91582ddf96db01e2a
anh1910493 <104414856+anh1910493@users.noreply.github.com>
2022-10-30 08:56:41 +0700

    Xay dung trang chi tiet san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:myshop/ui/products/edit_product_screen.dart';
+import 'package:provider/provider.dart';
+import 'ui/screens.dart';
+
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -10,21 +14,127 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
-      title: 'My Shop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(  
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-        ).copyWith(
-          secondary: Colors.deepOrange,
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => AuthManager(),
+          ),
+          ChangeNotifierProxyProvider<AuthManager, ProductsManager>(
+            create: (ctx) => ProductsManager(),
+            update: (ctx, authManager, productsManager) {
+              productsManager!.authToken = authManager.authToken;
+              return productsManager;
+            },
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
+          ),
+        ],
+        child: Consumer<AuthManager>(
+          builder: (ctx, authManager, child) {
+            return MaterialApp(
+              title: 'My Shop',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                  fontFamily: 'Lato',
+                  colorScheme:
+                      ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                          .copyWith(
+                    secondary: Colors.deepOrange,
+                  )),
+              home: authManager.isAuth
+                  ? const ProductsOverviewScreen()
+                  : FutureBuilder(
+                      future: authManager.tryAutoLogin(),
+                      builder: (ctx, snapshot) {
+                        return snapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? const SplashScreen()
+                            : const AuthScreen();
+                      },
+                    ),
+              routes: {
+                CartScreen.routeName: (ctx) => const CartScreen(),
+                OrderScreen.routeName: (ctx) => const OrderScreen(),
+                UserProductsScreen.routeName: (ctx) =>
+                    const UserProductsScreen(),
+              },
+              onGenerateRoute: (settings) {
+                if (settings.name == ProductDetailScreen.routeName) {
+                  final productId = settings.arguments as String;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return ProductDetailScreen(
+                        ctx.read<ProductsManager>().findById(productId),
+                      );
+                    },
+                  );
+                }
+
+                if (settings.name == EditProductScreen.routeName) {
+                  final productId = settings.arguments as String?;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return EditProductScreen(
+                        productId != null
+                            ? ctx.read<ProductsManager>().findById(productId)
+                            : null,
+                      );
+                    },
+                  );
+                }
+                return null;
+              },
+            );
+          },
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
-    
-    home: Container(  
-      color: Colors.green,
-    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
-  } 
+  }
 }
